b3045f981fc1d0f998ecb0032b300e70b6b3655f
boxnaster <nastya3362@yandex.ru>
2024-06-10 00:45:13 +0200

    Implemented shot details

--- Analogia/Views/DetailView.swift
+++ Analogia/Views/DetailView.swift
@@ -8,9 +8,44 @@
 import Foundation
 import SwiftUI
 
+var shot = Shot(objectID: "1",
+                name: "Cow",
+                date: Date.now,
+                image: Image("Cow"),
+                location: "Lazar utca 9",
+                film: "Kodak Gold 200",
+                aperture: 5.6,
+                shutterSpeed: "1/1000",
+                iso: 400,
+                focus: 7.5,
+                note: "I used exposure compensation",
+                flashIsUsed: false)
+
 struct DetailView: View {
     var body: some View {
-            Text("No data")
+        ScrollView {
+            VStack {
+                shot.image
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                VStack(alignment: .leading, spacing: 10) {
+                    Text("Name: \(shot.name)")
+                    Text("Date: \(shot.date.formatted())")
+                    Text("Location: \(shot.location)")
+                    Text("Film: \(shot.film)")
+                    Text("ISO: \(shot.iso)")
+                    Text("Aperture: \(shot.aperture.formatted())")
+                    Text("Shutter Speed: \(shot.shutterSpeed)")
+                    Text("Focus: \(shot.focus.formatted())")
+                    if shot.flashIsUsed {
+                        Text("Flash: Yes")
+                    } else {
+                        Text("Flash: No")
+                    }
+                    Text("Note: \(shot.note)")
+                }
+            }
+        }
     }
 }
 

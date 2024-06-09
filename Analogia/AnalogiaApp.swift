58f5da02b0e2c35f4cb8c48d46790b4b38fe213d
boxnaster <nastya3362@yandex.ru>
2024-06-09 19:48:55 +0200

    Initial Commit

---
+++ Analogia/AnalogiaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnalogiaApp.swift
+//  Analogia
+//
+//  Created by Анастасия Тимофеева on 09.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct AnalogiaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

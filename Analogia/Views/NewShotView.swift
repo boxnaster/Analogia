7bc74078e156a8026a22d251ac909dbb1f994786
boxnaster <nastya3362@yandex.ru>
2024-07-09 23:21:44 +0200

    Added the possibility to take a photo

--- Analogia/Views/NewShotView.swift
+++ Analogia/Views/NewShotView.swift
@@ -18,6 +18,10 @@ struct NewShotView: View {
     @State private var isFlashUsed: Bool = false
     @State private var note: String = ""
 
+    @State private var showCamera = false
+    @State private var selectedImage: UIImage?
+    @State var image: UIImage?
+
     var location: String = "O utca 9" // need to get the location
 
     let apertureValues = ["None", "1.8", "2.0", "5.6"]
@@ -34,6 +38,33 @@ struct NewShotView: View {
     var body: some View {
         NavigationView {
             Form {
+
+                if let selectedImage{
+                    Image(uiImage: selectedImage)
+                        .resizable()
+                        .scaledToFit()
+
+                } else {
+
+                    Section {
+                        Button("+ Take Photo") {
+                            self.showCamera.toggle()
+                        }
+                        .padding()
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 10)
+                                .stroke(Color.blue, lineWidth: 1)
+                        )
+                        .frame(maxWidth: .infinity, alignment: .center)
+                        .fullScreenCover(isPresented: self.$showCamera) {
+                            accessCameraView(selectedImage: self.$selectedImage)
+                        }
+                        .ignoresSafeArea()
+                    }
+                    .frame(height: 200)
+                }
+
+
                 Section {
                     TextField("Title", text: $name)
                     Text(location)
@@ -79,23 +110,63 @@ struct NewShotView: View {
                 }
                 .frame(height: 200, alignment: .topLeading)
             }
-                .navigationTitle("New Shot")
-                .navigationBarTitleDisplayMode(.inline)
-                .toolbar(content: {
-                    ToolbarItem(placement: .topBarTrailing) {
-                        Button("Done") {
-                            // Save the data
-                        }
+            .navigationTitle("New Shot")
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar(content: {
+                ToolbarItem(placement: .topBarTrailing) {
+                    Button("Done") {
+                        // Save the data
                     }
-                    ToolbarItem(placement: .topBarLeading) {
-                        Button("Cancel") {
-                            dismiss()
-                        }
+                }
+                ToolbarItem(placement: .topBarLeading) {
+                    Button("Cancel") {
+                        dismiss()
                     }
-                })
+                }
+            })
         }
     }
 }
+
+
+struct accessCameraView: UIViewControllerRepresentable {
+
+    @Binding var selectedImage: UIImage?
+    @Environment(\.presentationMode) var isPresented
+
+    func makeUIViewController(context: Context) -> UIImagePickerController {
+        let imagePicker = UIImagePickerController()
+        imagePicker.sourceType = .camera
+        imagePicker.allowsEditing = true
+        imagePicker.delegate = context.coordinator
+        return imagePicker
+    }
+
+    func updateUIViewController(_ uiViewController: UIImagePickerController, context: Context) {
+
+    }
+
+    func makeCoordinator() -> Coordinator {
+        return Coordinator(picker: self)
+    }
+}
+
+// Coordinator will help to preview the selected image in the View.
+class Coordinator: NSObject, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+    var picker: accessCameraView
+
+    init(picker: accessCameraView) {
+        self.picker = picker
+    }
+
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        guard let selectedImage = info[.originalImage] as? UIImage else { return }
+        self.picker.selectedImage = selectedImage
+        self.picker.isPresented.wrappedValue.dismiss()
+    }
+}
+
+
 
 #Preview {
     NewShotView()
